2203b26f24007316bd6e7e0e52c1b1623663da9d
wxy <1044393721@qq.com>
2017-06-05 22:04:21 +0800

    caodan

--- app/src/main/java/com/example/wxy/myapplication/MyApplicatiomn.java
+++ app/src/main/java/com/example/wxy/myapplication/MyApplicatiomn.java
@@ -19,7 +19,6 @@ public class MyApplicatiomn extends Application {
         System.out.println("34");
         System.out.println("12");
         System.out.println("34");
-        System.out.println("12");
-        System.out.println("34");
+
     }
 }
